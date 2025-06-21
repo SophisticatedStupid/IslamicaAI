b49ff134b0d121c3445bca391c2d8b52232d7daa
Khan Tahsin Abrar <tahsinp12@gmail.com>
2025-06-21 18:42:35 +0000

    pt 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/header";
 import NoticeBoard from "./components/NoticeBoard";
 import ChatBox from "./components/chatbox";
